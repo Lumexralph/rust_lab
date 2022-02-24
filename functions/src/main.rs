de580d039e2c8df1e2fa5158dc161a97764a6037
Olumide Ogundele <olumideralph@gmail.com>
2022-02-24 13:12:16 +0100

    clean up

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -3,10 +3,10 @@ mod closures;
 
 
 fn main() {
-    // functions::display_functions_and_methods();
+    functions::display_functions_and_methods();
     functions::display_high_order_function();
 
-    // closures::display_closure_capturing();
-    // closures::display_closure_as_input_parameters();
-    // closures::display_closure_as_output();
+    closures::display_closure_capturing();
+    closures::display_closure_as_input_parameters();
+    closures::display_closure_as_output();
 }
